3aae52131f0f4011de47ab8edcaa329da896fc32
MaxStienitzka <maximilian.stienitzka03@htl-salzburg.ac.at>
2021-01-18 10:10:40 +0100

    Random Value Button + Function added, Return Value Text added, Screen idependency updated and fixed

---
+++ Assets/MyGame/RandomValuee.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomValuee : MonoBehaviour
+{
+    public float number;
+    public Text value;
+    
+
+   public void ValueButton()
+   {
+        number = UnityEngine.Random.value;
+        value.text = number.ToString();
+    }
+}
